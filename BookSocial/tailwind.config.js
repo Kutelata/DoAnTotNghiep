22ba5b75545c94031aaa3380a9e0b6e449e3ea5b
HoangCaoLong <longhc@dantri.net.vn>
2022-02-16 20:57:02 +0700

    Init

---
+++ BookSocial/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './BookSocial.Presentation/Views/**/*.cshtml',
+    './BookSocial.Presentation/Views/**/*.html',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
