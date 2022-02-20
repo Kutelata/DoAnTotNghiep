4b20a18e3deeaeb5a135b2ae504ae7df842b32de
HoangCaoLong <hclong2k@gmail.com>
2022-02-20 22:48:11 +0700

    Init

---
+++ Application/BookSocial/BookSocial.Presentation.Admin/Controllers/InboxController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookSocial.Presentation.Admin.Controllers
+{
+    public class InboxController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Application/BookSocial/BookSocial.Presentation.Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookSocial.Presentation.Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
